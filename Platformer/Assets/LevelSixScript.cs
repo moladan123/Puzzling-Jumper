750b9596a474e00296dd2580c6e4d6483ce40ec8
moladan123 <alon.djurinsky@gmail.com>
2018-01-24 22:33:05 -0500

    Finishing Level 6

---
+++ Platformer/Assets/LevelSixScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelSixScript : MonoBehaviour {
+
+    Text t;
+	// Use this for initialization
+	void Start () {
+        t = GameObject.Find("InputField").GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (t.text == "it" || t.text == "It")
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+	}
+}
